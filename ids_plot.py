8b771aa821389f383d0f42710af5dd12f8c8d73a
Aidan Gray <aidancgray@gmail.com>
2025-12-05 14:20:20 -0500

    added z axis

--- ids_plot.py
+++ ids_plot.py
@@ -1,220 +1,166 @@
+import os
 import sys
 import time
 import csv
-import serial
 import datetime as dt
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-from math import floor
-from serial.serialutil import SEVENBITS, PARITY_ODD, STOPBITS_ONE
-import IDS
+import IDSlib.IDS as IDS
 
-DATA_PATH = "/Users/aidancgray/Documents/MIRMOS/DiFCS/attocube/"
+GET_IDS = True
+
 IDS_IP = "172.16.1.198"
 ANIM_INTER = 500
-DATA_LIMIT = 300
-SER_DIF = '/dev/tty.usbserial-A506NMAT'
-SER_HTR = '/dev/tty.usbserial-14420'
+DATA_LIMIT = 100
+
+if os.name == "posix":
+    DATA_PATH = "/Users/aidancgray/Documents/MIRMOS/DiFCS/testdata/"
+else: 
+    DATA_PATH = "C:/Users/Aidan/Documents/MIRMOS/DIFCs_Testing/"
 
 DEBUG = sys.argv[1] if len(sys.argv) > 1 else None
 
 # This function is called periodically from FuncAnimation
-def animate(i, xs, tds, ths, dys):
-    # Get temp and position data
-    temp_dif = get_Lakeshore_temp(ser_dif) if SER_DIF else None
-    temp_htr = get_Lakeshore_temp(ser_htr) if SER_HTR else None
-
+def animate(i, t, ids_x, ids_y, ids_z):
     try:
-        warningNo, pos_1_pm, pos_2_pm, pos_3_um = ids.displacement.getAbsolutePositions()
-
-        abs_1_um = float(pos_1_pm) / 1000000
-        abs_2_um = float(pos_2_pm) / 1000000
-
+        (warningNo, pos_1_pm, pos_2_pm, pos_3_pm) = ids.displacement.getAbsolutePositions() if GET_IDS else (None, 0, 0, 0)
         pos_1_um = float(pos_1_pm - start_1) / 1000000
         pos_2_um = float(pos_2_pm - start_2) / 1000000
-
-        delta_length_um = -1*(pos_1_um + pos_2_um)
+        pos_3_um = float(pos_3_pm - start_3) / 1000000
 
         # Add x and y to lists
         meas_time = float("{0:.3f}".format((dt.datetime.now() - start_time).total_seconds()))
         
         data_tmp = [meas_time, 
-                    temp_dif if temp_dif else '--', 
-                    temp_htr if temp_htr else '--', 
-                    abs_1_um, 
-                    abs_2_um]
+                    pos_1_um,
+                    pos_2_um,
+                    pos_3_um,
+                    ]
 
-        xs.append(floor(meas_time))
-        tds.append(temp_dif) if temp_dif else None
-        ths.append(temp_htr) if temp_htr else None 
-        dys.append(delta_length_um)
+        t.append(meas_time)
+
+        ids_x.append(pos_1_um)
+        ids_y.append(pos_2_um)
+        ids_z.append(pos_3_um)
 
         # Limit x and y lists to DATA_LIMIT items
-        xs = xs[-DATA_LIMIT:]
-        tds = tds[-DATA_LIMIT:]
-        ths = ths[-DATA_LIMIT:]
-        dys = dys[-DATA_LIMIT:]
+        t = t[-DATA_LIMIT:]
+
+        ids_x = ids_x[-DATA_LIMIT:]
+        ids_y = ids_y[-DATA_LIMIT:]
+        ids_z = ids_z[-DATA_LIMIT:]
     
     except ValueError:
         return None
 
     else:
-        fig.clear()  # clear
-        ax1, ax2, ax3 = setup_plots()
+        fig.clear()
+        ax1 = setup_plots()
 
         # Draw x and y lists
         marker_fmt = '.'
         ms_fmt = 5
         lw_fmt = 1
-        if SER_DIF:
-            linetd, = ax1.plot(xs, tds, marker=marker_fmt, markersize=ms_fmt, 
-                               linewidth=lw_fmt, color='orange')
-        if SER_HTR:
-            lineth, = ax2.plot(xs, ths, marker=marker_fmt, markersize=ms_fmt, 
-                               linewidth=lw_fmt, color='purple')
-        line1, = ax3.plot(xs, dys, marker=marker_fmt, markersize=ms_fmt, 
-                          linewidth=lw_fmt, color='blue')
 
-        if SER_DIF:
-            xy_pos_td = (1.01, 0.95)
-            ax1.annotate(f'{temp_dif} K', xy=xy_pos_td, xycoords='axes fraction',
-                         size=10, ha='left', va='top',
-                         bbox=dict(boxstyle='round', fc='w'))
-        if SER_HTR:
-            xy_pos_th = (1.01, 0.95)
-            ax2.annotate(f'{temp_htr} K', xy=xy_pos_th, xycoords='axes fraction',
-                         size=10, ha='left', va='top',
-                         bbox=dict(boxstyle='round', fc='w'))
+        l_ids_x, = ax1.plot(t, ids_x, marker=marker_fmt, markersize=ms_fmt, linewidth=lw_fmt, color='red')
+        l_ids_y, = ax1.plot(t, ids_y, marker=marker_fmt, markersize=ms_fmt, linewidth=lw_fmt, color='blue')
+        l_ids_z, = ax1.plot(t, ids_z, marker=marker_fmt, markersize=ms_fmt, linewidth=lw_fmt, color='green')
+
+        xy_pos_0 = (1.01, 0.95)
+        xy_pos_1 = (1.01, 0.70)
+        xy_pos_2 = (1.01, 0.45)
+        xy_pos_3 = (1.01, 0.20)
+        xy_pos_4 = (1.01, -0.05)
         
-        xy_pos = (1.01, 0.95)
-        xy_pos_time = (1.01, 0.70)
-        ax3.annotate(f'{delta_length_um} um', xy=xy_pos, xycoords='axes fraction',
-                     size=10, ha='left', va='top',
+        ax1.annotate(f'x: {"{0:.3f}".format(pos_1_um)}', xy=xy_pos_0, xycoords='axes fraction',
+                     size=10, ha='left', va='top', color='red', 
                      bbox=dict(boxstyle='round', fc='w'))
-        ax3.annotate(f'{floor(meas_time)} s', xy=xy_pos_time, xycoords='axes fraction',
-                     size=10, ha='left', va='top',
+        ax1.annotate(f'y: {"{0:.3f}".format(pos_2_um)}', xy=xy_pos_1, xycoords='axes fraction',
+                     size=10, ha='left', va='top', color='blue', 
                      bbox=dict(boxstyle='round', fc='w'))
+        ax1.annotate(f'z: {"{0:.3f}".format(pos_3_um)}', xy=xy_pos_2, xycoords='axes fraction',
+                     size=10, ha='left', va='top', color='green', 
+                     bbox=dict(boxstyle='round', fc='w'))
+        
         plt.draw()
         append_to_csv(dataFile, data_tmp)
 
-        return line1, linetd if SER_DIF else None, lineth if SER_HTR else None
+        return l_ids_x, l_ids_y, l_ids_z
 
 def setup_plots():
-    gs = fig.add_gridspec(3, 1, wspace=0, hspace=0.1)
-    ax1 = fig.add_subplot(gs[0, 0])
-    ax2 = fig.add_subplot(gs[1, 0])
-    ax3 = fig.add_subplot(gs[2, 0])
-
+    # gs = fig.add_gridspec(1, 1, wspace=0, hspace=0.1)
+    ax1 = fig.add_subplot()
     ax1.grid()
-    ax2.grid()
-    ax3.grid()
 
-    # axt.set_title('Axes Positions vs Time')
-    ax1.set_ylabel('Piezo Temp (K)')
-    ax1.tick_params(labelbottom=False)
-    
-    ax2.set_ylabel('Coldplate Temp (K)')
-    ax2.tick_params(labelbottom=False)
-    
-    ax3.set_xlabel('Elapsed Time (s)')
-    ax3.set_ylabel(r'$\Delta$Piezo Length (um)')
-    ax3.tick_params(axis='x', labelrotation=45)
-    
-    return ax1, ax2, ax3
+    ax1.set_ylabel(r'pos (um)')
+    ax1.tick_params(axis='x', labelrotation=45)
+    ax1.set_xlabel('Elapsed Time (s)')
+
+    return ax1 
 
 def append_to_csv(dataFile, data):
-    with open(f'{dataFile}', 'a') as fd:
+    with open(f'{dataFile}', 'a', newline='') as fd:
         writer = csv.writer(fd)
         writer.writerow(data)
 
-def get_Lakeshore_temp(ser):
-    msg = ('CDAT?').encode()
-    ser.write(msg)
-    resp = ser.readline()
-    ls_temp = resp.decode()
-    ls_temp = ls_temp.split(' ')[0]
-    try:
-        ls_temp = float(ls_temp)
-    except ValueError:
-        return -999
-    else:
-        return ls_temp
-
 if __name__ == "__main__":
     dataFile = f"{DATA_PATH}{dt.datetime.now().strftime('%d%m%Y_%H-%M-%S')}.csv"
-    header = ['time', 'temp_dif', 'temp_htr', 'axis_1', 'axis_2']
-    if SER_HTR and SER_DIF:
-        ser_htr = serial.Serial(port=SER_HTR, 
-                                baudrate=1200, 
-                                timeout=1, 
-                                bytesize=SEVENBITS,
-                                parity=PARITY_ODD,
-                                stopbits=STOPBITS_ONE)
-    
-        ser_dif = serial.Serial(port=SER_DIF, 
-                                baudrate=1200, 
-                                timeout=1, 
-                                bytesize=SEVENBITS,
-                                parity=PARITY_ODD,
-                                stopbits=STOPBITS_ONE)
-    elif SER_DIF:
-        ser_dif = serial.Serial(port=SER_DIF, 
-                                baudrate=1200, 
-                                timeout=1, 
-                                bytesize=SEVENBITS,
-                                parity=PARITY_ODD,
-                                stopbits=STOPBITS_ONE)
-    elif SER_HTR:
-        ser_htr = serial.Serial(port=SER_HTR, 
-                                baudrate=1200, 
-                                timeout=1, 
-                                bytesize=SEVENBITS,
-                                parity=PARITY_ODD,
-                                stopbits=STOPBITS_ONE)
+    header = ['time', 
+              'ids_x', 
+              'ids_y',
+              'ids_z',]
 
-    ids = IDS.Device(IDS_IP)
-    ids.connect()
-    
-    if not ids.displacement.getMeasurementEnabled():
-        ids.system.setInitMode(0) # enable high accuracy mode
-        ids.system.startMeasurement()
-        while not ids.displacement.getMeasurementEnabled():
-            time.sleep(1)
+    if GET_IDS:
+        ids = IDS.Device(IDS_IP) 
+        ids.connect()
+        
+        if not ids.displacement.getMeasurementEnabled():
+            ids.system.setInitMode(0) # enable high accuracy mode
+            ids.system.startMeasurement()
+            while not ids.displacement.getMeasurementEnabled():
+                time.sleep(1)
 
     print(f'dataFile: {dataFile}')
     append_to_csv(dataFile, header)
 
     # Create figure for plotting
     fig = plt.figure()
-    ax1, ax2, ax3 = setup_plots()
+    ax1 = setup_plots()
 
-    xs = []
-    tds = []
-    ths = []
-    dys = []
+    t = [] 
+    ids_x, ids_y, ids_z = [], [], []
 
     # Draw x and y lists
     marker_fmt = '.'
     ms_fmt = 10
     lw_fmt = 1
-    if SER_DIF:
-        linetd, = ax1.plot(xs, tds, marker=marker_fmt, markersize=ms_fmt, 
-                           linewidth=lw_fmt, color='orange')
-    if SER_HTR:
-        lineth, = ax2.plot(xs, ths, marker=marker_fmt, markersize=ms_fmt, 
-                           linewidth=lw_fmt, color='purple')
-    line1, = ax3.plot(xs, dys, marker=marker_fmt, markersize=ms_fmt, 
-                      linewidth=lw_fmt, color='blue')
 
+    l_ids_x, = ax1.plot(t, ids_x, marker=marker_fmt, markersize=ms_fmt, linewidth=lw_fmt, color='red')
+    l_ids_y, = ax1.plot(t, ids_y, marker=marker_fmt, markersize=ms_fmt, linewidth=lw_fmt, color='blue')
+    l_ids_z, = ax1.plot(t, ids_z, marker=marker_fmt, markersize=ms_fmt, linewidth=lw_fmt, color='green')
+
+    # Get starting values
     start_time = dt.datetime.now()
-    warningNo, start_1, start_2, start_3 = ids.displacement.getAbsolutePositions()
-    start_t = get_Lakeshore_temp(ser_dif) if SER_DIF else None
+    
+    (warningNo, start_1, start_2, start_3) = ids.displacement.getAbsolutePositions() if GET_IDS else (None, 0, 0, 0)
+    
+    try:
+        # Set up plot to call animate() function periodically
+        ani = animation.FuncAnimation(fig, 
+                                      animate, 
+                                      fargs=(t, ids_x, ids_y, ids_z), 
+                                      interval=ANIM_INTER,
+                                      cache_frame_data=False)
 
-    # Set up plot to call animate() function periodically
-    ani = animation.FuncAnimation(fig, 
-                                    animate, 
-                                    fargs=(xs, tds, ths, dys), 
-                                    interval=ANIM_INTER,
-                                    cache_frame_data=False)
-    plt.show()
-    ids.close()
+        manager = plt.get_current_fig_manager()
+        if os.name == "posix":
+            manager.full_screen_toggle()
+        else:    
+            manager.resize(1280, 720)
+        plt.show()
+        plt.pause(.01)
+    except KeyboardInterrupt:
+        print("kb_int")
+    finally:
+        print("closing")
+        sys.exit(0)
