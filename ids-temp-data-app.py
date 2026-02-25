b87076019910af9af4626acd3e753da9051902af
Aidan Gray <aidancgray@gmail.com>
2026-02-24 20:33:26 -0500

    IDS + Temperature Data using Shiny

---
+++ ids-temp-data-app.py
@@ -0,0 +1,96 @@
+from pathlib import Path
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import deque
+from io import StringIO
+from shiny import reactive
+from shiny.express import render, ui
+from shiny.session import session_context
+
+if os.name == "posix":
+    DATA_PATH = "/Users/aidancgray/Documents/MIRMOS/DiFCS/testdata/"
+else:
+    DATA_PATH = "C:/Users/Aidan/Documents/MIRMOS/DIFCs_Testing/"
+DATA_LIMIT = 50
+data_dir = Path(DATA_PATH)
+data_file = max([f for f in data_dir.glob("*.csv")], key=lambda item: item.stat().st_ctime)
+
+ui.page_opts(fillable=True)
+
+{"class": "bslib-page-dashboard"}
+with session_context(None):
+
+    @reactive.file_reader(data_dir / data_file)
+    def data_df():
+        fname = data_dir / data_file
+        with open(fname, 'r') as f:
+            q = [ f.readline() ] 
+            q.extend(deque(f, DATA_LIMIT)) 
+        data_df = pd.read_csv(StringIO(''.join(q)))
+        return data_df
+
+with ui.layout_columns(col_widths=[ 10, 2],):
+    with ui.card():
+        @render.plot(alt='Data Plot')
+        def plot_data():
+            fig = plt.figure()
+            gs = fig.add_gridspec(1, 1, wspace=0, hspace=0)
+            ax1 = fig.add_subplot(gs[0, 0])
+            ax1.grid()
+        
+            ax1.set_ylabel(r'pos (um)')
+            ax1.tick_params(axis='x', labelrotation=45)
+            ax1.set_xlabel('Elapsed Time (s)')
+            
+            # Draw x and y lists
+            marker_fmt = '.'
+            ms_fmt = 5
+            lw_fmt = 1
+
+            raw_df = data_df()
+            t = raw_df["time"]
+            ids_x = raw_df["ids_x_0"]
+            ids_y = raw_df["ids_y_0"]
+            ids_z = raw_df["ids_z_0"]
+
+            t = t[-DATA_LIMIT:]
+            ids_x = ids_x[-DATA_LIMIT:]
+            ids_y = ids_y[-DATA_LIMIT:]
+            ids_z = ids_z[-DATA_LIMIT:]
+
+            l_ids_x, = ax1.plot(t, ids_x, marker=marker_fmt, markersize=ms_fmt, linewidth=lw_fmt, color='red')
+            l_ids_y, = ax1.plot(t, ids_y, marker=marker_fmt, markersize=ms_fmt, linewidth=lw_fmt, color='blue')
+            l_ids_z, = ax1.plot(t, ids_z, marker=marker_fmt, markersize=ms_fmt, linewidth=lw_fmt, color='green')
+        
+            return fig
+
+    with ui.card():        
+        @render.data_frame
+        def df():
+            raw_df = data_df()
+            x_ids_df = raw_df["ids_x_0"].iloc[-1]
+            y_ids_df = raw_df["ids_y_0"].iloc[-1]
+            z_ids_df = raw_df["ids_z_0"].iloc[-1]
+            try:
+                sp_df    = raw_df["setpoint"].iloc[-1]
+                dac_x_df    = raw_df["dac_x"].iloc[-1]
+                dac_y_df    = raw_df["dac_y"].iloc[-1]
+            except:  # noqa: E722
+                df = pd.DataFrame(np.array([
+                    ['IDS X', "{0:.3f}".format(x_ids_df)],
+                    ['IDS Y', "{0:.3f}".format(y_ids_df)],
+                    ['IDS Z', "{0:.3f}".format(z_ids_df)],
+                ]), columns=['KEY', 'VALUE'])    
+            else:
+                df = pd.DataFrame(np.array([
+                    ['SETPOINT', "{0:.3f}".format(sp_df)],
+                    ['DAC X', dac_x_df],
+                    ['IDS X', "{0:.3f}".format(x_ids_df)],
+                    ['DAC Y', dac_y_df],
+                    ['IDS Y', "{0:.3f}".format(y_ids_df)],
+                    ['IDS Z', "{0:.3f}".format(z_ids_df)],
+                ]), columns=['KEY', 'VALUE'])
+            
+            return df
---
+++ ids_temp_noplot.py
@@ -0,0 +1,149 @@
+import os
+import sys
+import time
+import csv
+import serial
+import datetime as dt
+from serial.serialutil import SEVENBITS, PARITY_ODD, STOPBITS_ONE
+import IDSlib.IDS as IDS
+from lakeshore import Model336
+
+GET_COUNTS = False
+GET_MAG = False
+GET_IDS = True
+GET_OP = False
+GET_TEMPS = True
+
+IDS_IP = "172.16.1.198"
+DATA_ACQ_RATE = 400
+
+if os.name == "posix":
+    DATA_PATH = "/Users/aidancgray/Documents/MIRMOS/DiFCS/testdata/"
+    SER_MAG = "/dev/tty.usbserial-BG01GH9Y"
+    SER_HTR = "/dev/tty.usbserial-A506NMAT"
+else: 
+    DATA_PATH = "C:/Users/Aidan/Documents/MIRMOS/DIFCs_Testing/"
+    SER_MAG = 'COM3'
+    SER_HTR = 'COM10' 
+
+DEBUG = sys.argv[1] if len(sys.argv) > 1 else None
+
+# This function is called periodically from FuncAnimation
+def dataLoop():
+    global data_count
+
+    # Get temp and position data
+    temp_htr = get_Lakeshore_temp(ser_htr) if SER_HTR else 0
+    temp_a, temp_b, temp_c, temp_d = ls_366.get_all_kelvin_reading()[:4] if GET_TEMPS else (0,0,0,0)
+
+    try:
+        (warningNo, pos_1_pm, pos_2_pm, pos_3_pm) = ids.displacement.getAbsolutePositions() if GET_IDS else (None, 0, 0, 0)
+        abs_1_um = float(pos_1_pm) / 1000000
+        abs_2_um = float(pos_2_pm) / 1000000
+        abs_3_um = float(pos_3_pm) / 1000000
+        
+        ids_x_0 = -abs_1_um + (float(start_1) /  1000000)
+        ids_y_0 =  abs_2_um - (float(start_2) /  1000000)
+        ids_z_0 =  abs_3_um - (float(start_3) /  1000000)
+        
+        # Add x and y to lists
+        meas_time = float("{0:.3f}".format((dt.datetime.now() - start_time).total_seconds()))
+        
+        data_tmp = [meas_time, 
+                    temp_htr,
+                    temp_a,
+                    temp_b,
+                    temp_c,
+                    temp_d,
+                    abs_1_um,
+                    abs_2_um,
+                    abs_3_um,
+                    ids_x_0,
+                    ids_y_0,
+                    ids_z_0,
+                    ]
+        if (DEBUG != 'no-write'):
+            append_to_csv(dataFile, data_tmp)
+        data_count+=1
+
+    except ValueError:
+        return None
+
+def append_to_csv(dataFile, data):
+    with open(f'{dataFile}', 'a', newline='') as fd:
+        writer = csv.writer(fd)
+        writer.writerow(data)
+
+def get_Lakeshore_temp(ser):
+    msg = ('CDAT?\n').encode()
+    ser.write(msg)
+    resp = ser.readline()
+    ls_temp = resp.decode()
+
+    ls_temp = ls_temp.split(' ')[0]
+    try:
+        ls_temp = float(ls_temp)
+    except ValueError:
+        return -999
+    else:
+        return ls_temp
+
+if __name__ == "__main__":
+    dataFile = f"{DATA_PATH}{dt.datetime.now().strftime('%d%m%Y_%H-%M-%S')}_ids_temp_{DEBUG}.csv"
+    header = ['time',
+              'temp_htr', 
+              'temp_a', 
+              'temp_b', 
+              'temp_c', 
+              'temp_d', 
+              'ids_x', 
+              'ids_y',
+              'ids_z',
+              'ids_x_0', 
+              'ids_y_0',
+              'ids_z_0',]
+    
+    if SER_HTR:
+        ser_htr = serial.Serial(port=SER_HTR, 
+                                baudrate=1200, 
+                                timeout=1, 
+                                bytesize=SEVENBITS,
+                                parity=PARITY_ODD,
+                                stopbits=STOPBITS_ONE)
+    
+    ls_366 = Model336()
+
+    if GET_IDS:
+        ids = IDS.Device(IDS_IP) 
+        ids.connect()
+        
+        if not ids.displacement.getMeasurementEnabled():
+            ids.system.setInitMode(0) # enable high accuracy mode
+            ids.system.startMeasurement()
+            while not ids.displacement.getMeasurementEnabled():
+                time.sleep(1)
+    
+    print(f'dataFile: {dataFile}')
+    append_to_csv(dataFile, header)
+
+    # Get starting values
+    start_time = dt.datetime.now()
+    
+    start_t_htr = get_Lakeshore_temp(ser_htr) if SER_HTR else 0
+    start_t_a, start_t_b, start_t_c, start_t_d = ls_366.get_all_kelvin_reading()[:4]
+    
+    (warningNo, start_1, start_2, start_3) = ids.displacement.getAbsolutePositions() if GET_IDS else (None, 0, 0, 0)
+    
+    data_count = 0
+    time_start = time.perf_counter()
+    try:
+        while True:
+            dataLoop()
+            time.sleep(DATA_ACQ_RATE/1000)
+    except KeyboardInterrupt:
+        print("kb_int")
+    finally:
+        time_stop = time.perf_counter()
+        print(f"{data_count} / {'{0:.2f}'.format(time_stop-time_start)}")
+        print("closing")
+        sys.exit(0)
