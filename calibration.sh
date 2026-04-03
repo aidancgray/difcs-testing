e8422922e1684dc0d35fd1386f6ac699424d7a9d
Aidan Gray <aidancgray@gmail.com>
2026-04-03 12:41:13 -0400

    scripts

---
+++ calibration.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+python noplot_pid.py 1
+sleep 10
+python noplot_pid.py 2
+sleep 10
+python noplot_pid.py 1
+sleep 10
+python noplot_pid.py 2
+sleep 10
---
+++ circle-plots.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+python noplot_circle_25.py
+sleep 10
+python noplot_circle_75.py
+sleep 10
+python noplot_circle_50.py
+sleep 10
+python noplot_circle_25.py
+sleep 10
+python noplot_circle_75.py
+sleep 10
+python noplot_circle_50.py
